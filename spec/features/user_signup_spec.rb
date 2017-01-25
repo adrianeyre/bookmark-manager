55b266bf5242248ef5ea2bbf20ca0080fb4232f5
Joe Marriott <marriott90@gmail.com>
2017-01-25 19:48:47 +0000

    preventing duplicate registrations

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -8,14 +8,24 @@ feature 'User sign up' do
   scenario 'Requires confirmation password to match' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq('/users')
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
 
   scenario 'Requires email input to sign up' do
     expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email must not be blank')
   end
 
   scenario 'Requires valid email address to sign up' do
     expect { sign_up(email: 'invalid@example') }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email has an invalid format')
+  end
+
+  scenario 'Unable to sign up if email already used' do
+    sign_up
+    expect { sign_up }.to_not change(User, :count)
+    expect(page).to have_content('Email is already taken')
   end
 end
