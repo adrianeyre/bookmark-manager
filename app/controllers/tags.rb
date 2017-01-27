61990d5f52cd70803535789de575a612675fc216
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-27 14:48:54 +0000

    Updated css

--- app/controllers/tags.rb
+++ app/controllers/tags.rb
@@ -1,6 +1,20 @@
 class BookmarkManager < Sinatra::Base
   get '/tags/:search' do
-    tag = Tag.first(name: params[:search].downcase)
+    session[:search] = params[:search]
+    search
+  end
+
+  get '/tags' do
+    erb :'tags/index'
+  end
+
+  post '/tags' do
+    session[:search] = params[:search]
+    search
+  end
+
+  def search
+    tag = Tag.first(name: session[:search].downcase)
     @links = tag ? tag.links : []
     erb :'links/index'
   end
--- spec/features/resetting_password_spec.rb
+++ spec/features/resetting_password_spec.rb
@@ -11,7 +11,7 @@ feature 'Resetting Password' do
  scenario 'When I forget my password I can see a link to reset' do
    visit '/sessions/new'
    click_link 'I forgot my password'
-   expect(page).to have_content("Please enter your email address")
+   expect(page).to have_content("Password Recovery")
  end
 
  scenario 'When I enter my email I am told to check my inbox' do
