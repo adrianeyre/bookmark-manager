9faa6e55404a5d29cac9854793fede33db52310e
Joe Marriott <marriott90@gmail.com>
2017-01-27 13:16:59 +0000

    app.rb routing logic divided in app/controllers

---
+++ app/server.rb
@@ -0,0 +1,16 @@
+class BookmarkManager < Sinatra::Base
+  set :views, File.dirname(__FILE__) + '/views'
+  use Rack::MethodOverride
+  register Sinatra::Flash
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $PROGRAM_NAME
+end
